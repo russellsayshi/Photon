739bf112c236a5a67742d2c09ddc1304687efa6c
Jimmy Beckett <jimmybeckett@Jimmy-MA.local>
2018-03-08 23:54:59 -0600

    Made some more things const

--- Vec3/Vec3.h
+++ Vec3/Vec3.h
@@ -10,12 +10,12 @@ class Vec3 {
 public:
     //constructors
     Vec3() : x(0), y(0), z(0) {}
-    Vec3(float x, float y, float z) : x(x), y(y), z(z) {}
+    Vec3(const float x, const float y, const float z) : x(x), y(y), z(z) {}
     
     //getters
-    float getX() { return x; }
-    float getY() { return y; }
-    float getZ() { return z; }
+    float getX() const { return x; }
+    float getY() const { return y; }
+    float getZ() const { return z; }
 
     //setters
     void setX(float x) { this->x = x; }
@@ -23,23 +23,23 @@ public:
     void setZ(float z) { this->z = z; }
 
     //operators
-    Vec3 operator+(const Vec3& other) { return Vec3 (this->x + other.x, this->y + other.y, this->z + other.z); } //addition
+    Vec3 operator+(const Vec3& other) const { return Vec3 (this->x + other.x, this->y + other.y, this->z + other.z); } //addition
     Vec3& operator+=(const Vec3& other) { return *this = *this + other; } //compound assignment addition
-    Vec3 operator-(const Vec3& other) { return Vec3 (this->x - other.x, this->y - other.y, this->z - other.z); } //subtraction
+    Vec3 operator-(const Vec3& other) const { return Vec3 (this->x - other.x, this->y - other.y, this->z - other.z); } //subtraction
     Vec3& operator-=(const Vec3& other) { return *this = *this - other; } //compound assignment subtraction
-    Vec3 operator*(const float f) { return Vec3 (this->x * f, this->y * f, this->z * f); } //multiplication by scalar
+    Vec3 operator*(const float f) const { return Vec3 (this->x * f, this->y * f, this->z * f); } //multiplication by scalar
     Vec3& operator*=(const float f) { return *this = *this * f; } //compound assignment multiplication
-    bool operator==(const Vec3& other) { return abs(this->x - other.x) < .001 && abs(this->y - other.y) < .001 && abs(this->z - other.z) < .001; } //equality
-    bool operator!=(const Vec3& other) { return !(*this == other); } //not equal to
+    bool operator==(const Vec3& other) const { return abs(this->x - other.x) < .001 && abs(this->y - other.y) < .001 && abs(this->z - other.z) < .001; } //equality
+    bool operator!=(const Vec3& other) const { return !(*this == other); } //not equal to
     friend std::ostream& operator<<(std::ostream& os, const Vec3& v) { return os << "[" << v.x << ", " << v.y << ", " << v.z << "]"; } //stream insertion
     
     //dot and cross products
-    float dot(const Vec3& other) { return this->x * other.x + this->y * other.y + this->z * other.z; } //dot product
-    Vec3 cross(const Vec3& other) { return Vec3 (this->y * other.z - this->z * other.y, this->z * other.x - this->x * other.z, this->x * other.y - this->y * other.x); } //cross product
+    float dot(const Vec3& other) const { return this->x * other.x + this->y * other.y + this->z * other.z; } //dot product
+    Vec3 cross(const Vec3& other) const { return Vec3 (this->y * other.z - this->z * other.y, this->z * other.x - this->x * other.z, this->x * other.y - this->y * other.x); } //cross product
 
     //other methods
-    float mag() { return sqrt(this->x * this->x + this->y * this->y + this->z * this->z); } //returns the magnitude
-    Vec3 norm() { float l = this->mag(); return Vec3 (this->x / l, this->y / l, this->z / l); } //returns a normalized vector
+    float mag() const { return sqrt(this->x * this->x + this->y * this->y + this->z * this->z); } //returns the magnitude
+    Vec3 norm() const { float l = this->mag(); return Vec3 (this->x / l, this->y / l, this->z / l); } //returns a normalized vector
 };
 
 #endif
