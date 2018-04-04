d5850b151ea5eda0898f8df115716d9630485eb1
Russell Coleman <russellsayshi@gmail.com>
2018-04-03 21:45:39 -0500

    Update most_basic_possible_sdl.c

--- most_basic_possible_sdl.c
+++ most_basic_possible_sdl.c
@@ -4,6 +4,23 @@
 //from the sdl tutorial
 //and tweaked
 
+struct win {
+	SDL_Surface* surface;
+	SDL_PixelFormat* format;
+	SDL_Window* window;
+};
+
+void set_pixel_raw(SDL_Surface *surface, int x, int y, Uint32 pixel)
+{
+	Uint8 *target_pixel = (Uint8 *)surface->pixels + y * surface->pitch + x * 4;
+	*(Uint32 *)target_pixel = pixel;
+}
+
+void set_pixel(win* window, int x, int y, Uint8 r, Uint8 g, Uint8 b) {
+	Uint32 pixel = SDL_MapRGB(const SDL_PixelFormat* window->format, r, g, b);
+	set_pixel_raw(window->surface, x, y, pixel);
+}
+
 int main(int argc, char* argv[]) {
 
 	SDL_Window *window;
@@ -22,6 +39,18 @@ int main(int argc, char* argv[]) {
 		printf("Something is very wrong: %s\n", SDL_GetError());
 		return 1;
 	}
+	
+	SDL_Surface *screen;
+	screen = SDL_GetWindowSurface(window);
+	SDL_PixelFormat *format = screen->format;
+	SDL_UpdateWindowSurface(window);
+	
+	win* window = new win();
+	window->surface = screen;
+	window->window = window;
+	window->format = format;
+	set_pixel(window, 10, 10, 0, 255, 0); //set pixel (10, 10) to bright green
+	delete window;
 
 	// Wait a lil, then close window
 	SDL_Delay(3000);
