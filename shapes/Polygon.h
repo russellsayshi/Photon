27e9a4234794e15fcb1069df8342d0148afb20c2
sam_alvis <alvibry@yahoo.com>
2018-05-01 09:58:29 -0500

    finished converting plane and polygon class to use dot and cross products

--- shapes/Polygon.h
+++ shapes/Polygon.h
@@ -18,14 +18,15 @@ public:
 			: normal(new Vec3(a, b, c)), dotBoi(d) {}
 
 	Polygon(double a, double b, double c, double x, double y, double z) : Polygon(a, b, c, 0) {
-		double d = a * x + b * y + c * z;
-		dotBoi = d;
+		Vec3* tv = new Vec3(a,b,c);
+		Vec3* tp = new Vec3(x,y,z);
+		dotBoi = (double)tv->dot(*tp);
 	}
 
 	Polygon(double x1, double y1, double z1, double x2, double y2, double z2, double x3, double y3, double z3) {
 		Vec3* ab = new Vec3(x2-x1, y2-y1, z2-z1);
 		Vec3* ac = new Vec3(x3-x1, y3-y1, z3-z1);
-		
+		normal = ab->cross(*ac)
 	}
 
 	bool intersects(Ray *ray);
