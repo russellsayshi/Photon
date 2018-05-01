3b7660f360b896222092fac20a3872d9845641bb
sam_alvis <alvibry@yahoo.com>
2018-05-01 09:50:06 -0500

    Began using dot products and cross products to define polygon and plane classes

--- shapes/Polygon.h
+++ shapes/Polygon.h
@@ -17,9 +17,15 @@ public:
 	Polygon(double a, double b, double c, double d)
 			: normal(new Vec3(a, b, c)), dotBoi(d) {}
 
-	Polygon(double a, double b, double c, double x, double y, double z) : Plane(a, b, c, 0) {
+	Polygon(double a, double b, double c, double x, double y, double z) : Polygon(a, b, c, 0) {
 		double d = a * x + b * y + c * z;
 		dotBoi = d;
+	}
+
+	Polygon(double x1, double y1, double z1, double x2, double y2, double z2, double x3, double y3, double z3) {
+		Vec3* ab = new Vec3(x2-x1, y2-y1, z2-z1);
+		Vec3* ac = new Vec3(x3-x1, y3-y1, z3-z1);
+		
 	}
 
 	bool intersects(Ray *ray);
